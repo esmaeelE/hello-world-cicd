1898ff6ffa41a03f09807e9e0b34a5e05e28f30e
esmaeel <a@b.com>
2026-09-18 21:25:45 +0330

    little change app.py

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World from GitLab CI/CD!\n"
+    return "Hello World from GitLab CI/CD! is changes\n"
 
 
 @app.route("/health")
 def health():
-    return "OK\n"
+    return "OK!!!\n"
 
 
 if __name__ == "__main__":
